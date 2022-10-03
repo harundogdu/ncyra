7b48d9bd3f7558dba55fa9a5873fdb3a887a2caf
Harun Doğdu <Harundogdu06@gmail.com>
2022-10-03 15:52:09 +0300

    Updates

---
+++ precache-manifest.6e6c1244a5371cf291c6a381586c84c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000af5984701594ba7526a386b11f671",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c91bf4cd132abcf3896",
+    "url": "./static/css/main.db691fc5.chunk.css"
+  },
+  {
+    "revision": "e19bf1821e02e0e6c769",
+    "url": "./static/js/2.e1ab5d13.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e1ab5d13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c91bf4cd132abcf3896",
+    "url": "./static/js/main.42963c51.chunk.js"
+  },
+  {
+    "revision": "90128fa87f74b698edb6",
+    "url": "./static/js/runtime-main.22d9c8b1.js"
+  }
+]);
